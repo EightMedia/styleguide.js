76ebe19ad281d887217334fb223d6791ee083946
Arjen Scherff-de Water <aratramba@gmail.com>
2013-11-07 11:49:21 +0100

    added test for references; moved tests around

--- test/styleguide_test.js
+++ test/styleguide_test.js
@@ -9,9 +9,7 @@ var $, sg;
 exports.styleguide = {
 
   setUp: function(done) {
-    sg = new StyleGuide('Styleguide Demo')
-    sg.parseFile("test/fixtures/style.css")
-    sg.includeJS("test/fixtures/script.js")
+    
     done();
   },
 
@@ -19,6 +17,10 @@ exports.styleguide = {
   // default
   default: function(test) {
     test.expect(1);
+
+    sg = new StyleGuide('Styleguide Demo')
+    sg.parseFile("test/fixtures/default/style.css")
+    sg.includeJS("test/fixtures/default/script.js")
     sg.renderToFile("test/tmp/index.html")
 
     var actual = grunt.file.read('test/tmp/index.html');
@@ -41,11 +43,37 @@ exports.styleguide = {
   custom_css: function(test) {
     test.expect(1);
 
-    sg.customCSS("test/fixtures/template/styleguide.css")
-    sg.renderToFile("test/tmp/custom.html")
+    sg = new StyleGuide('Styleguide Demo')
+    sg.parseFile("test/fixtures/default/style.css")
+    sg.customCSS("test/fixtures/custom-css/styleguide.css")
+    sg.renderToFile("test/tmp/custom-css.html")
 
-    var actual = grunt.file.read('test/tmp/custom.html');
-    var expected = grunt.file.read('test/expected/custom.html');
+    var actual = grunt.file.read('test/tmp/custom-css.html');
+    var expected = grunt.file.read('test/expected/custom-css.html');
+
+    $ = cheerio.load(expected);
+    $('time').remove();
+    expected = $.html();
+
+    $ = cheerio.load(actual);
+    $('time').remove();
+    actual = $.html();
+
+    test.equal(actual, expected, 'should be able to insert custom css');
+    test.done();
+  },
+
+
+  // references
+  references: function(test) {
+    test.expect(1);
+
+    sg = new StyleGuide('Styleguide Demo')
+    sg.parseFile("test/fixtures/references/style.css")
+    sg.renderToFile("test/tmp/references.html")
+
+    var actual = grunt.file.read('test/tmp/references.html');
+    var expected = grunt.file.read('test/expected/references.html');
 
     $ = cheerio.load(expected);
     $('time').remove();
