dfc6f4ca8851decc92532905b0afff5b7a47eb98
J. Tangelder <j.tangelder@gmail.com>
2014-06-25 16:19:05 +0200

    guides sorted by section/title. performance improvement by only loading the section that is targeted

--- test/simple.js
+++ test/simple.js
@@ -1,7 +1,9 @@
 var StyleGuide = require('../main.js');
 
 var sg = new StyleGuide();
-sg.addFile("test/fixtures/includes/style1.css");
+sg.addFile("test/tmp/main.css");
+sg.addFile("test/fixtures/references/style.css");
+sg.addFile("test/fixtures/template/style.css");
 sg.addFile("test/fixtures/includes/style2.css");
 sg.render({ outputFile: 'test/expected/includes.html'}, function() {
         //console.log(arguments);
